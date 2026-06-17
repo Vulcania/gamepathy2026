97451fac28ae2e893fe9c5e4c421ea6e584c99b8
Níke <145349804+solarcrow@users.noreply.github.com>
2026-06-17 17:10:16 +0200

    feat: started hit- and hurtboxes
    or something like that, maybe

--- health/healthbar.gd
+++ health/healthbar.gd
@@ -1,11 +1,10 @@
-extends Control
+extends Node2D
 
 @onready var max_bar_fill = %healthbar.size.x
 var bar_fill_amount : float
 
-func update_healthbar(current_health, max_health):
-	bar_fill_amount = (float(current_health) / max_health) * max_bar_fill
+func update_healthbar(_current_health : int, _max_health : int):
+	bar_fill_amount = (float(_current_health) / _max_health) * max_bar_fill
 	if bar_fill_amount > max_bar_fill:
 		bar_fill_amount = max_bar_fill
 	%healthbar.size.x = bar_fill_amount
-	
