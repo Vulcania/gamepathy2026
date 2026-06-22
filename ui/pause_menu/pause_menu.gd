082a57b0084d29d7b7ec23c38f5e7358bd3aa66f
artisticrow <imanoeztuerk@gmail.com>
2026-06-22 15:31:18 +0200

    i dont know, just trial and error

--- ui/pause_menu/pause_menu.gd
+++ ui/pause_menu/pause_menu.gd
@@ -4,6 +4,9 @@ extends Control
 
 func _ready() -> void:
 	quit.visible = false
+	hide()
+	if Input.is_action_pressed("pause"):
+		show()
 
 func _on_music_h_slider_value_changed(value: float) -> void:
 	Options.change_music_volume(value)
@@ -12,13 +15,13 @@ func _on_sfx_h_slider_value_changed(value: float) -> void:
 	Options.change_sfx_volume(value)
 
 func _on_close_pause_pressed() -> void:
-	$".".visible = false
+	hide()
 
 func _on_quit_run_pressed() -> void:
 	quit.visible = true
 
 func _on_yes_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://ui/title_screen/title_screen.tscn")
 
 func _on_no_pressed() -> void:
 	quit.visible = false
