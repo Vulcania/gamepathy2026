11dcb7d56387f9dbb7f5292ec7171f5e0531033d
artisticrow <imanoeztuerk@gmail.com>
2026-06-27 18:14:49 +0200

    idk

--- GLOBALS/Global.gd
+++ GLOBALS/Global.gd
@@ -1,27 +1,8 @@
 extends Node
 
-var negative_time = false
-
 func _ready():
 	if Dialogic.current_timeline != null:
 		return
 
 func open_buff_options():
-	pass
-
-func long_talk():
-	pass
-	#lose more time
-
-func short_talk():
-	pass
-	#lose less time
-
-func buff_one():
-	pass
-
-func buff_two():
-	pass
-
-func buff_three():
 	pass
