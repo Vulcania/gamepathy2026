23f10a3f8fb1f6a2f543a0ccaa0147aa7541775d
artisticrow <imanoeztuerk@gmail.com>
2026-06-27 16:10:24 +0200

    Dialog per BuffSelection

--- ui/buffs/buff_selection.gd
+++ ui/buffs/buff_selection.gd
@@ -1,17 +1,23 @@
 extends Control
 class_name BuffSelection
 
+var agatha
+var player
+
 var buff_list : Array[TextureRect]
 
 func _ready() -> void:
-	pass # Replace with function body.
+	agatha = get_node("res://npc/AgathaToad/agatha.tscn")
+	player = get_node("res://player/player.tscn")
 
 func _on_option_1_pressed() -> void:
-	Agatha.option_1()
-	pass # Replace with function body.
+	Dialogic.start("res://dialog/timelines/Agatha_Option1.dtl")
+	agatha.option_1()
 
 func _on_option_2_pressed() -> void:
-	pass # Replace with function body.
+	Dialogic.start("res://dialog/timelines/Agatha_Option2.dtl")
+	agatha.option_2()
 
 func _on_option_3_pressed() -> void:
-	pass # Replace with function body.
+	Dialogic.start("res://dialog/timelines/Agatha_Option3.dtl")
+	agatha.option_3()
