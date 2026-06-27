9fd73af423d8f11a45b1e4c41257c4d798d304e9
Níke <145349804+solarcrow@users.noreply.github.com>
2026-06-27 18:01:44 +0200

    Deer Attack Animation

--- scripts/attack_box.gd
+++ scripts/attack_box.gd
@@ -2,7 +2,3 @@ extends Area2D
 class_name AttackBox
 
 @export var damage : int = 1
-
-func _init() -> void:
-	collision_layer = 2
-	collision_mask = 0
--- scripts/hit_box.gd
+++ scripts/hit_box.gd
@@ -3,10 +3,6 @@ extends Area2D
 
 signal take_damage
 
-func _init() -> void:
-	collision_layer = 0
-	collision_mask = 2
-
 func _on_area_entered(hitbox : AttackBox) -> void:
 	if hitbox == null:
 		return
