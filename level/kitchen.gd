20ce88b6d271e60d3a7ad7ff5790b4c066aebd9c
artisticrow <imanoeztuerk@gmail.com>
2026-07-12 16:49:44 +0200

    dialogs work

--- level/kitchen.gd
+++ level/kitchen.gd
@@ -3,11 +3,12 @@ extends Node2D
 var is_in_area = false
 
 func _ready() -> void:
-	pass # Replace with function body.
+	$Door/pressE.hide()
 
 func _input(event):
-	if event.is_action_pressed("interact"):
-		if is_in_area:
+	if is_in_area:
+		$Door/pressE.show()
+		if event.is_action_pressed("interact"):
 			$Door/AnimationPlayer.play("door_opened")
 
 func enter_level_two():
@@ -16,7 +17,9 @@ func enter_level_two():
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Player:
 		is_in_area = true
+		$Door/pressE.show()
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	if area.get_parent() is Player:
 		is_in_area = false
+		$Door/pressE.hide()
