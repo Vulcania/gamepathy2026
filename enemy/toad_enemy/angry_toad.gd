0a9cfb74278cb49ba2b7ac5e5b5c72d9a67bcf35
artisticrow <imanoeztuerk@gmail.com>
2026-08-17 18:25:53 +0200

    angry toad

--- enemy/toad_enemy/angry_toad.gd
+++ enemy/toad_enemy/angry_toad.gd
@@ -4,8 +4,8 @@ extends StaticBody2D
 
 var dead = false
 
-var max_health = 5
-var current_health = 5
+var max_health = 3
+var current_health = 3
 var hit = false
 
 func _ready():
@@ -14,17 +14,20 @@ func _ready():
 
 func _on_detect_player_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Player && !dead && !hit:
+		print("toad detects player")
 		animation.play("Attack")
 		await get_tree().create_timer(2).timeout
 
 func _on_detect_player_area_exited(area: Area2D) -> void:
 	if area.get_parent() is Player && !dead && !hit:
 		animation.play("Idle")
+		print("player left me, the sequel by toad")
 
 func get_hit():
 	hit = !hit
 	
 	if hit && !dead:
+		print("toad got hit, health left: ", current_health)
 		animation.play("Hit")
 		current_health -= 1
 		get_node("Healthbar").update_healthbar(current_health, max_health)
@@ -33,4 +36,14 @@ func get_hit():
 			die()
 
 func die():
+	print("toad died")
 	animation.play("Death")
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	if area.get_parent() is Player and !dead:
+		get_hit()
+
+func _on_attack_box_area_entered(area: Area2D) -> void:
+	if area.get_parent() is Player and !hit:
+			print("toad area attacking player")
+			area.get_parent().take_damage(1)
